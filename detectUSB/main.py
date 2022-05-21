b3a773e548fc35ff97fa1bdae137ec96ccaee0ad
IVainqueur <ishimvainqueur@gmail.com>
2022-05-21 12:40:14 +0200

    Adding sub directories

--- detectUSB/main.py
+++ detectUSB/main.py
@@ -1,8 +1,18 @@
+'''
+    This program has Timer that checks every 100 milliseconds if the /media/pi folder is populated.
+    IF YES: that means there is an external device connected to the Raspberry Pi so turn the connectedLED on
+    ELSE: just turn off the connectedLED and turn on the errorPin
+
+'''
+
+
+
 import requests
 from threading import Timer
 import time
 import os
 import RPi.GPIO as GPIO
+import atexit
 
 # Setting up the board and GPIO Related Stuff
 GPIO.setwarnings(False)
@@ -11,7 +21,7 @@ GPIO.setmode(GPIO.BOARD)
 # Renaming some variables for convinience
 HIGH = GPIO.HIGH
 LOW = GPIO.LOW
-OUTPUT = GPIO.OUTPUT
+OUTPUT = GPIO.OUT
 INPUT = GPIO.IN
 digitalWrite = GPIO.output
 
@@ -24,8 +34,15 @@ errorPin = 5
 
 # Setting the LEDs
 GPIO.setup(connectedPin, OUTPUT, initial=LOW)
-GPIO.setup(errorPin, OUTPUT, initial=LOW)
+GPIO.setup(errorPin, OUTPUT, initial=HIGH)
 
+# Function to switch off the LEDS at exit
+def turnOffLeds():
+    print("======== BYE ===========")
+    digitalWrite(connectedPin, LOW)
+    digitalWrite(errorPin, LOW)
+    
+atexit.register(turnOffLeds)
 
 def checkUSB():
     global prevConnection
@@ -44,4 +61,5 @@ def checkUSB():
     Timer(0.1, checkUSB).start()
 
 checkUSB()
+
 
